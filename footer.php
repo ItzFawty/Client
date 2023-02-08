3bd1214f09e36ff33731773fcca09f1b5e78047b
Tomaš Zbedina <tomaszbedina@gmail.com>
2023-02-08 18:04:31 +0100

    side & navbar

--- footer.php
+++ footer.php
@@ -1,2 +1,26 @@
+
+</div>
+
+
 </body>
+<script>
+    var app = new Vue({
+        el: "#app",
+        data: {
+            navpulldown: true,
+            users: [
+                {
+                    Name: "Andrew Tate",
+                    ID: "101010"
+                },
+            ]
+        }
+    })
+
+
+    for(i = 0; i < 10; i++)
+    {
+        app.$data.users.push({Name: "Ande" + i + 1, ID: (i+1)*100})
+    }
+</script>
 </html>
